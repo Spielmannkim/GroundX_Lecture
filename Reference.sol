2d3db316265cee8c576a31d1439c0fa212c3a435
shinjong kim <63417540+Spielmannkim@users.noreply.github.com>
2022-12-12 21:01:10 +0900

    Reference 타입

--- Reference.sol
+++ Reference.sol
@@ -2,3 +2,7 @@
 // storage = 영속적으로 저장되는 영역에 저장
 // calldata = external함수 인자에 사용되는 공간
 // 레퍼런스 타입들은 코딩을 하면서 더 익혀보자.
+
+// 서로 다른 영역을 침범할 시 데이터 복사
+// storage -> memory/ calldata
+// anything -> storage
